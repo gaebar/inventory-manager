42f1cbd793f7d9a21ff9d8cf9b28ab4f7488a466
Gaetano <gaetanobarreca@gmail.com>
2023-10-11 00:18:48 -0700

    Added not null value to schema database.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,9 +3,9 @@ DROP TABLE IF EXISTS products;
 CREATE TABLE products (
     product_id BIGINT PRIMARY KEY AUTO_INCREMENT,
     product_name VARCHAR(100),
-    expiry_date DATE,
-    time_duration_for_markdown INT,
-    min_threshold INT,
-    max_threshold INT,
-    current_stock INT
+    expiry_date DATE NOT NULL,
+    time_duration_for_markdown INT NOT NULL,
+    min_threshold INT NOT NULL,
+    max_threshold INT NOT NULL,
+    current_stock INT NOT NULL
 );
