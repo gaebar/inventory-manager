c131f511c09e2a99d94bc61c1bbf0315392a02db
Gaetano <gaetanobarreca@gmail.com>
2023-10-26 10:48:07 -0700

    Refactoring project

--- src/main/java/com/inventory/inventorymanager/utility/DatabaseUtility.java
+++
@@ -1,6 +0,0 @@
-package com.inventory.inventorymanager.utility;
-
-// A utility class for database operations that don't neatly fit into Spring Data JPA's model, or for database setup/teardown.
-public class DatabaseUtility {
-    
-}
--- src/main/java/com/inventory/inventorymanager/utility/DateUtility.java
+++
@@ -1,5 +0,0 @@
-package com.inventory.inventorymanager.utility;
-
-// Utility class for any date operations, like calculating expiry and markdown dates.
-public class DateUtility {
-}
